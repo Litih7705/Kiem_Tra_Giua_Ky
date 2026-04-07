e88fdcdc2397d66736a2e18077134d1f51f79265
Thanh <030239230213@st.buh.edu.vn>
2026-04-07 09:21:49 +0700

    Thực hiện Menu thể loại + Chi tiết phim

--- app/Http/Controllers/MovieController2.php
+++ app/Http/Controllers/MovieController2.php
@@ -1,21 +1,24 @@
 <?php
 namespace App\Http\Controllers;
-use App\Models\Movie; 
+use App\Models\Movie;
 use Illuminate\Http\Request;
+
 
 class MovieController2 extends Controller {
     // 1. Lọc phim theo thể loại (Mục 2 - Ý 2)
     public function getCategory($id) {
         $movies = Movie::whereHas('genres', function($query) use ($id) {
-            $query->where('genre.id', $id); 
+            $query->where('genre.id', $id);
         })
         ->where('status', 1) // Chỉ lấy phim chưa xóa mềm
         ->orderBy('release_date', 'desc')
         ->limit(12)
         ->get();
 
+
         return view('category', compact('movies'));
     }
+
 
     // 2. Trang chi tiết phim (Mục 2 - Ý 4)
     public function getDetail($id) {
@@ -25,3 +28,4 @@ class MovieController2 extends Controller {
         return view('detail.index', compact('movie'));
     }
 }
+
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,7 +1,10 @@
 <x-movie-layout title="Danh sách phim theo thể loại">
     <div class="list-movie"> @foreach($movies as $movie)
             <div class="movie"> <a href="{{ route('movie.detail', $movie->id) }}">
-                    <img src="{{ $movie->image_link }}" width="100%">
+                    <img src="{{ asset('storage' . $movie->image) }}"
+                        class="img-fluid rounded shadow-lg w-100"
+                        alt="{{ $movie->movie_name_vn }}"
+                        onerror="this.src='{{ $movie->image_link }}'">
                     <div class="movie-info p-2"> <h6>{{ $movie->movie_name_vn }}</h6>
                         <small class="text-muted">{{ $movie->release_date }}</small>
                     </div>
@@ -10,3 +13,10 @@
         @endforeach
     </div>
 </x-movie-layout>
+
+
+
+
+
+
+
